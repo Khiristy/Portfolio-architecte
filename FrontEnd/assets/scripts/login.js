45f9c0a498ef50e49e6d490c28971de9dc5f8bca
khiristy <victorien.delmotte.59300@gmail.com>
2023-12-15 18:03:47 +0100

    update login.js

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,23 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // apiLogin()
+    // apiLogin()
 
-  const formLogin = document.getElementById("form_login");
+    const formLogin = document.getElementById("formlogin");
 
-  formLogin.addEventListener("submit", async function (e) {
-    const login = {
-      email: "sophie.bluel@test.tld",
-      password: "S0phie",
-    };
+    formLogin.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        const email = document.getElementById("email")
+        const password = document.getElementById("password")
 
-    e.preventDefault();
-    console.log(e);
-    let response = await fetch("/api/users/login", {
-      method: "POST",
-      body: new FormData(formLogin),
+        const login = {
+            email: email.value,
+            password: password.value,
+        };
+
+
+        console.log(e);
+        let response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(login),
+            //   body: new FormData(formLogin),
+        });
+
+        let result = await response.json();
+        console.log(result);
+        window.localStorage.setItem("tokenConnexion", result.token);
+        window.localStorage.setItem("userId", result.userId);
+
+        if (result.token !== undefined) {
+           const loginResult = document.getElementById('loginResult')
+            loginResult.innerText = "Vous etes maintenant connecté"
+            setTimeout(() => {
+
+                window.location.href = "index.html"
+            }, 3000);
+            
+        }
+        else {
+            loginResult.innerText = "L'email ou le mot de passe est incorrect"
+        }
     });
-
-    let result = await response.json();
-    console.log(result);
-    alert(result.message);
-  });
 });
+
+
+
+
+
+
+
+
+
+
+
+// email: 'sophie.bluel@test.tld',
+//     password: 'S0phie',
--- FrontEnd/assets/scripts/requestApi.js
+++ FrontEnd/assets/scripts/requestApi.js
@@ -107,6 +107,8 @@ function apiPostWorks() {
     .catch(error => console.error('Erreur :', error));
 
 }
+
+
 // apiPostWorks()
 
 
