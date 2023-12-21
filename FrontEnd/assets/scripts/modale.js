c12b9e18fb8f87e010e243c36341b4db83583554
Khiristy <147670388+Khiristy@users.noreply.github.com>
2023-12-21 19:01:34 +0100

    update modale & add requestWork/Delete

--- FrontEnd/assets/scripts/modale.js
+++ FrontEnd/assets/scripts/modale.js
@@ -1,4 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    Modale.init();
+})
 
+
+//ouverture/fermeture modale
 const Modale = {
     init: function () {
         this.modale = document.getElementById('modale')
@@ -9,17 +14,104 @@ const Modale = {
     },
     event: function () {
         const that = this
-        modaleOpen.addEventListener('click', function () {
+        this.modaleOpen.addEventListener('click', function () {
             that.modale.classList.add('modale_show')
             that.body.classList.add('overlay')
         })
-        modaleClose.addEventListener('click', function () {
+        this.modaleClose.addEventListener('click', function () {
             that.modale.classList.remove('modale_show')
             that.body.classList.remove('overlay')
         })
-    } 
+    }
 }
 
+//modifie contenu modale
 
-Modale.init();
+const ProjectEditor = {
+    init: function () {
+
+    },
+
+    request: function () {
+
+        const url = 'http://localhost:5678/api/works';
+
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+                ProjectEditor.buildGallery(data)
+                ProjectEditor.deleteWorks(data)
+            })
+            .catch(error => console.error('Erreur :', error));
+    },
+
+    buildGallery: function (data) {
+        const modaleGallery = document.getElementById('modaleGallery')
+        for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            const imgProject = element.imageUrl
+            const imgAlt = element.title
+            const imgPos = document.createElement("figure")
+            modaleGallery.appendChild(imgPos)
+            imgPos.classList.add('modale_gallery-pos')
+
+            const image = document.createElement("img")
+            console.log(imgProject);
+            image.src = imgProject
+            image.alt = imgAlt
+            imgPos.appendChild(image)
+
+            const trashImg = document.createElement("i")
+            trashImg.classList.add("fa-solid")
+            trashImg.classList.add("fa-trash-can")
+            trashImg.classList.add("modale_gallery-trash")
+            imgPos.appendChild(trashImg)
+        }
+    },
+
+    deleteWorks: function (data) {
+
+        const trash = document.querySelectorAll(".modale_gallery-trash")
+        for (let index = 0; index < trash.length; index++) {
+            const element = trash[index];
+            trash[index].addEventListener('click', async function () {
+
+                const url = 'http://localhost:5678/api/users//works/{id}';
+                const token = window.localStorage.getItem("tokenConnexion")
+                const userId = window.localStorage.getItem("userId")
+
+                const login = {
+                    email: email.value,
+                    password: password.value,
+                };
+
+
+                console.log(e);
+                let response = await fetch("http://localhost:5678/api/users//works/{id}", {
+                    method: "DELETE",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(login),
+                    //   body: new FormData(formLogin),
+                });
+
+                fetch(url, requestOptions)
+                    .then(response => response.json())
+                    .then(data => console.log(data))
+                    .catch(error => console.error('Erreur :', error));
+            })
+        }
+    },
+}
+
+ProjectEditor.request()
 
